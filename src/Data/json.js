bbc16365f84a6ca56af7a4ff08ffc8bd70aa85de
Eimer Adames <eimeradames@gmail.com>
2020-12-30 22:44:32 -0500

    first

---
+++ src/Data/json.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        userId: 1,
+        image: "",
+        name: { first: "Johnny", last: "Silverhand" },
+        phone: "5122222222",
+        email: "Rockerboy@gmail.com",
+        dob: "10-16-1988"
+    },
+    {
+        userId: 2,
+        image: "",
+        name: { first: "Lucas", last: "Morris" },
+        phone: "123456",
+        email: "racks.jacson&#64;learningcontainer.com",
+        dob: "07-23-1990"
+    },
+    {
+        userId: 3,
+        image: "",
+        name: { first: "Sandy", last: "Ruedell" },
+        phone: "2354430",
+        email: "joe.jacson@learningcontainer.com",
+        dob: "07-23-2000"
+    }
+]
+
+export default persons;
